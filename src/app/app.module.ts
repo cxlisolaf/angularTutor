95c44d983f64da170cba2919dc6e54b11231680f
Ellie <ellie@Ellies-MacBook-Pro.local>
2019-03-25 16:04:29 -0600

    homework2-1 cavas and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
+import {Homework2Component} from './homework2/homework2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    Homework2Component
   ],
   imports: [
     BrowserModule,
