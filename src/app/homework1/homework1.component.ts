76ce96664ddfc051d49a6a1468afd49e9dd8a246
Ellie <ellie@Ellies-MacBook-Pro.local>
2019-03-25 12:23:44 -0600

    homework1-2 sign in dialog

---
+++ src/app/homework1/homework1.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // select: '[app-homework1]',
+  selector: 'app-homework1',
+  templateUrl: './homework1.component.html', // you can put real html here
+  styleUrls: ['./homework1.component.css']
+})
+export class Homework1Component implements OnInit {
+  constructor() {}
+  ngOnInit() { }
+}
